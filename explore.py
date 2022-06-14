1f3f653a83ddcff791eb59249dc959eee660e6df
Sebas Moran <65722993+1xretr0@users.noreply.github.com>
2022-06-14 12:09:27 -0500

    changed real credentials to just place holders xd

--- explore.py
+++ explore.py
@@ -3,8 +3,8 @@
 from twilio.rest import Client
 
 client = Client(
-    "ACe7ecba6048ff3b33d7f068d9f99f112e",
-    "1173ed6f4ced8c73d3f232060529582a"
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN
 )
 
 for msg in client.messages.list():
